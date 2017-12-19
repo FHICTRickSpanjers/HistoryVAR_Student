ca2575b616ad59b1cba5737724ab0a525980f9d0
Ruben <rubenfalkenburg@gmail.com>
2017-12-19 11:33:58 +0100

    #1

---
+++ HistoryVAR/Assets/Scripts/Creator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace History_VAR.Classes
+{
+    class Creator
+    {
+        //Name, Gender, Bday, PlaceofBirth, Deathday, Deathplace
+        private string CreatorName;
+        private string CreatorGender;
+        private DateTime CreatorBday;
+        private string CreatorPlaceOfBirth;
+        private DateTime CreatorDeathDay;
+        private string CreatorPlaceOfDeath;
+
+        //Number of objects made, most famous art, creatorgenre
+        private int NumberOfArtObjects;
+        private string CreatorMostFamousArt;
+        private string CreatorGenre;
+        
+        //List of professions
+        private List<string> ListofProfessions = new List<string>();
+
+        //Description of the creator
+        private string CreatorDescription;
+
+
+        public Creator()
+        {
+
+        }
+    }
+}
